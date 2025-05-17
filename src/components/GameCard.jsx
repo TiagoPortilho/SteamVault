e36e61a96c0755a0ce5e4509b311efb931fd26df
Tiago <tiagoportilho52@gmail.com>
2025-05-16 21:44:59 -0300

    Bug fixes and home page assembly

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,32 +2,32 @@ import React from "react";
 import "../styles/GameCard.css";
 
 function GameCard({ game }) {
-    return (
-        <div class="card">
-            <img src={game.image} alt="Imagem do Jogo"></img>
-            <div class="game-title">
-                <h2>game.title</h2>
-            </div>
-            <div class="overlay">
-                <h2>game.title</h2>
-                <div class="info">
-                    <div class="info-item">
-                        <div class="info-label">Tempo Jogado</div>
-                        <div class="info-value">game.playtime</div>
-                    </div>
-                    <div class="info-item">
-                        <div class="info-label">Status</div>
-                        <div class="info-value">game.status</div>
-                    </div>
-                    <div class="info-item">
-                        <div class="info-label">Categoria</div>
-                        <div class="info-value">game.category</div>
-                    </div>
-                </div>
-                <a href="#" class="btn">Ver Detalhes</a>
-            </div>
+  return (
+    <div className="card">
+      <img src={game.image} alt="Imagem do Jogo" />
+      <div className="game-title">
+        <h2>{game.title}</h2>
+      </div>
+      <div className="overlay">
+        <h2>{game.title}</h2>
+        <div className="info">
+          <div className="info-item">
+            <div className="info-label">Tempo Jogado</div>
+            <div className="info-value">{game.playtime}</div>
+          </div>
+          <div className="info-item">
+            <div className="info-label">Status</div>
+            <div className="info-value">{game.status}</div>
+          </div>
+          <div className="info-item">
+            <div className="info-label">Categoria</div>
+            <div className="info-value">{game.category}</div>
+          </div>
         </div>
-    );
+        <a href="#" className="btn">Ver Detalhes</a>
+      </div>
+    </div>
+  );
 }
 
 export default GameCard;
